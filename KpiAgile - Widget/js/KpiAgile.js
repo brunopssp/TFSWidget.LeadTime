7e053a1b120257ec685d77fc8c00b4c4cc392d5d
Bruno Souza <brunopintosouza@gmail.com>
2016-08-02 01:05:36 -0300

    Add Validation to Done Items

--- KpiAgile - Widget/js/KpiAgile.js
+++ KpiAgile - Widget/js/KpiAgile.js
@@ -92,6 +92,14 @@ VSS.require(["TFS/Dashboards/WidgetHelpers", "TFS/WorkItemTracking/RestClient"],
 
 function ProcessRevisions(workItem) {
 
+    if (workItem[workItem.length - 1].fields["System.State"] != "Done") {
+
+        '#error'.empty();
+        $('h2.title').text(settings.queryPath.substr(15));
+        $('#query-info-container').empty().text("-");
+        $('#footer').empty().text("This query does not return any valid work item");
+        return;
+    }
     var RevApproved = workItem.find(function (workItemRevision) {
         return workItemRevision.fields["System.State"] == "Approved";
     });
