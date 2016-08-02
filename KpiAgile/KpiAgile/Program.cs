aa505fc4a0d1c7534b1646efdc50999812260340
Bruno Souza <brunopintosouza@gmail.com>
2016-08-01 22:56:49 -0300

    KPIAgileTJSP

--- KpiAgile/KpiAgile/Program.cs
+++ KpiAgile/KpiAgile/Program.cs
@@ -17,120 +17,158 @@ namespace KpiAgile
 
         static void Main(string[] args)
         {
-            string strStartStage = "Cópia e Ajustes";
-            string strSecondStage = "Implementar Design";
-            string strThirdStage = "Publicado em Dev";
+            string strStartStage = "1.Cópia e Ajustes";
+            string strSecondStage = "2.Implementar Design";
+            string strThirdStage = "3.Publicado em Dev";
+            string strFouthStage = "4.Validação de Conteúdo";
+            string strLastStage = "5.Disponibilizado Para Homologação UAT";
 
-            string strLastStage = "Disponibilizado Para Homologação UAT";
+            #region PorCiclo
+            Console.WriteLine("Selecione uma opção para medir o Cycle Time e Throughput:");
+            Console.WriteLine("{0} De: \"{1}\" Até: \"{2}\"", " [1]", strStartStage, strLastStage);
+            Console.WriteLine("{0} De: \"{1}\" Até: \"{2}\"", " [2]", strStartStage, strSecondStage);
+            Console.WriteLine("{0} De: \"{1}\" Até: \"{2}\"", " [3]", strStartStage, strThirdStage);
+            Console.WriteLine("{0} De: \"{1}\" Até: \"{2}\"", " [4]", strStartStage, strFouthStage);
+            Console.WriteLine("{0} De: \"{1}\" Até: \"{2}\"", " [5]", strSecondStage, strThirdStage);
+            Console.WriteLine("{0} \"{1}\"", " [6]", strStartStage);
+            Console.WriteLine("{0} \"{1}\"", " [7]", strSecondStage);
+            Console.WriteLine("{0} \"{1}\"", " [8]", strFouthStage);
 
-            Console.WriteLine("Selecione uma opção para medir o Cycle Time:");
-            Console.WriteLine("De: {0} Até: {1}", strStartStage, strSecondStage + " [1]");
-            Console.WriteLine("De: {0} Até: {1}", strStartStage, strThirdStage + " [2]");
-            Console.WriteLine("De: {0} Até: {1}", strStartStage, strLastStage + " [3]");
-            Console.WriteLine("De: {0} Até: {1}", strSecondStage, strThirdStage + " [4]");
             string strOpt = Console.ReadLine();
 
             switch (strOpt)
             {
                 case "1":
-                    CycleTime(strStartStage, strSecondStage);
+                    CycleTime(strStartStage, strSecondStage, strThirdStage, strFouthStage, strLastStage, false);
                     break;
                 case "2":
-                    CycleTime(strStartStage, strThirdStage);
+                    CycleTime(strStartStage, string.Empty, string.Empty, string.Empty, strSecondStage, false);
                     break;
                 case "3":
-                    CycleTime(strStartStage, strLastStage);
+                    CycleTime(strStartStage, string.Empty, string.Empty, string.Empty, strThirdStage, false);
                     break;
                 case "4":
-                    CycleTime(strSecondStage, strThirdStage);
+                    CycleTime(strStartStage, string.Empty, string.Empty, string.Empty, strFouthStage, false);
+                    break;
+                case "5":
+                    CycleTime(strSecondStage, string.Empty, string.Empty, string.Empty, strThirdStage, false);
+                    break;
+                case "6":
+                    CycleTime(strStartStage, string.Empty, string.Empty, string.Empty, strStartStage, true);
+                    break;
+                case "7":
+                    CycleTime(strSecondStage, string.Empty, string.Empty, string.Empty, strSecondStage, true);
+                    break;
+                case "8":
+                    CycleTime(strFouthStage, string.Empty, string.Empty, string.Empty, strFouthStage, true);
                     break;
                 default:
                     Console.WriteLine("Opção Invalida!");
                     Console.ReadLine();
                     break;
             }
+            #endregion
         }
 
-        public static void CycleTime(string startFieldValue, string endFieldValue)
+        public static void CycleTime(string strStartStage, string strSecondStage, string strThirdStage, string strFouthStage, string strLastStage, bool itemDone)
         {
             WorkItemTrackingHttpClient witClient;
-            WorkItemQueryResult result = GetQueryResult(out witClient, "Força Tarefa", "Páginas Estáticas");
-            if (result.WorkItems.Any())
+            WorkItemQueryResult queryResult = GetQueryResult(out witClient, "Força Tarefa", "Páginas Estáticas");
+            if (queryResult.WorkItems.Any())
             {
-                List<WorkItem> workItems = witClient.GetWorkItemsAsync(result.WorkItems.Select(wir => wir.Id)).Result;
-                //--Regra Geral--//
-                //List<decimal> decTempoPorPBI = new List<decimal>();
-                //--------------//
-                #region Migração
-                List<decimal> decPaginasPorDia = new List<decimal>();
-                List<decimal> decTempoPorPagina = new List<decimal>();
-                List<decimal> nTotalPaginas = new List<decimal>();
-                #endregion
+                List<WorkItem> workItems = witClient.GetWorkItemsAsync(queryResult.WorkItems.Select(wir => wir.Id)).Result;
+                List<double> nTotalPaginas = new List<double>();
+                List<double> nWIP = new List<double>();
+
                 string fieldName = "System.BoardColumn";
 
+                DateTime dtStartThroughput = DateTime.Today;
+                DateTime dtEndThroughput = new DateTime();
                 foreach (WorkItem workItem in workItems)
                 {
 
                     List<WorkItem> workItemsRevisions = witClient.GetRevisionsAsync((int)workItem.Id).Result;
-                    if (workItemsRevisions.Where(s => s.Fields[fieldName].Equals(startFieldValue)).Count() > 0)
+
+                    #region Throughput - WIP
+
+                    if (!workItem.Fields[fieldName].Equals(strLastStage))
                     {
-                        WorkItem workItemApproved = workItemsRevisions.Where(s => s.Fields[fieldName].Equals(startFieldValue)).First();
-                        if (workItemsRevisions.Where(s => s.Fields[fieldName].Equals(endFieldValue)).Count() > 0)
-                        {
-                            if (workItemsRevisions.Last().Fields[fieldName].ToString() != startFieldValue)
-                            {
-                                WorkItem workItemDone = workItemsRevisions.Where(s => s.Fields[fieldName].Equals(endFieldValue)).First();//Retorna a primeira revision que entrou no field
-
-                                ///Metricas--------
-                                DateTime dtStart = (DateTime)workItemApproved.Fields["System.ChangedDate"];
-                                DateTime dtEnd = (DateTime)workItemDone.Fields["System.ChangedDate"];
-
-                                TimeSpan difDate = dtEnd - dtStart;
-
-                                decimal horasUteis = ((Convert.ToDecimal(difDate.TotalHours)) - GetNumberOfWorkingHours(dtStart, dtEnd));
-                                decimal nPaginas = Convert.ToDecimal(workItemsRevisions.Last().Fields["Microsoft.VSTS.Scheduling.OriginalEstimate"]);
-
-                                #region Migração
-                                //--Regra Migração--//
-                                decPaginasPorDia.Add(nPaginas / (horasUteis / 24));
-                                decTempoPorPagina.Add(horasUteis / nPaginas);
-                                nTotalPaginas.Add(nPaginas);
-                                ///-------------///
-                                #endregion
-
-                                #region Regra Geral
-                                //--Regra Geral--//
-                                //decimal horasUteis = ((Convert.ToDecimal(difDate.TotalHours)) - GetNumberOfWorkingHours(dtStart, dtEnd));
-                                //decTempoPorPBI.Add(horasUteis);
-                                ///-------------///
-                                #endregion
-
-                                Console.WriteLine("{0} {1}", workItem.Id, workItem.Fields["System.Title"]);
-                            }
-                        }
+                        double nPaginasWIP = Convert.ToDouble(workItem.Fields["Microsoft.VSTS.Scheduling.OriginalEstimate"]);
+                        nWIP.Add(nPaginasWIP);//Throughput
                     }
+
+                    #endregion
+
+                    #region Validacoes Stages
+                    if (workItem.Fields[fieldName].Equals("0.Novo"))
+                        continue;
+                    if (workItemsRevisions.Where(s => s.Fields[fieldName].Equals(strStartStage) || s.Fields[fieldName].Equals(strStartStage.Remove(0, 2))).Count() == 0)//Valida se o PBI passou pelo stage Inicial
+                        continue;
+                    if (workItemsRevisions.Where(s => s.Fields[fieldName].Equals(strLastStage) || s.Fields[fieldName].Equals(strLastStage.Remove(0, 2))).Count() == 0)//Valida se o PBI chegou no stage Final
+                        continue;
+                    if (!itemDone)
+                        if (workItemsRevisions.Last().Fields[fieldName].ToString() == strStartStage || workItemsRevisions.Last().Fields[fieldName].ToString() == strStartStage.Remove(0, 2))//Valida se o PBI voltou ao stage inicial
+                            continue;
+                    #endregion
+
+
+                    WorkItem workItemApproved = workItemsRevisions.Where(s => s.Fields[fieldName].Equals(strStartStage) || s.Fields[fieldName].Equals(strStartStage.Remove(0, 2))).First();
+                    WorkItem workItemDone = workItemsRevisions.Where(s => s.Fields[fieldName].Equals(strLastStage) || s.Fields[fieldName].Equals(strLastStage.Remove(0, 2))).First();//Retorna a primeira revision que entrou no stage inicial
+
+                    #region Specific Cycle Stage
+
+                    if (itemDone)
+                    {
+                        workItemDone = workItemsRevisions.Where(s => s.Fields[fieldName].Equals(strLastStage) || s.Fields[fieldName].Equals(strLastStage.Remove(0, 2))).Last();//Retorna a última revision que estava no stage Final
+                        if (workItemDone.Fields["System.BoardColumnDone"] == null)
+                            continue;
+                    }
+
+                    #endregion
+
+                    double nPaginas = Convert.ToDouble(workItem.Fields["Microsoft.VSTS.Scheduling.OriginalEstimate"]);
+                    nTotalPaginas.Add(nPaginas);
+
+                    #region Throughput - Range date
+
+                    DateTime dtStart = (DateTime)workItemApproved.Fields["System.ChangedDate"];
+                    DateTime dtEnd = (DateTime)workItemDone.Fields["System.ChangedDate"];
+
+                    if (dtStartThroughput > dtStart)
+                        dtStartThroughput = dtStart;
+                    if (dtEndThroughput < dtEnd)
+                        dtEndThroughput = dtEnd;
+
+                    #endregion
+
+                    Console.WriteLine("{0} {1}", workItem.Id, workItem.Fields["System.Title"]);
                 }
-                #region Regra Geral
-                //--Regra Geral--//
-                //TimeSpan tsTempoPorPBI = DateTimeOffset.Now.AddHours(Convert.ToDouble(Math.Round(decTempoPorPBI.Average(), 2))) - DateTimeOffset.Now;
-                //Console.WriteLine("=======================================================");
-                //Console.WriteLine("De: {0} Até: {1}", startFieldValue, endFieldValue);
-                //Console.WriteLine("Quantidade de PBIs: {0}", decTempoPorPBI.Count);//Throughput
-                //Console.WriteLine("Média de tempo gasto por PBI: {0}", tsTempoPorPBI.ToString());//CycleTime
-                ///-------------///
-                #endregion
-                #region Migração
-                if (decPaginasPorDia.Count > 0)
+
+                #region Summary
+
+                Console.WriteLine("=======================================================");
+                Console.WriteLine("Quantidade de PBIs: {0}", nTotalPaginas.Count);
+                Console.WriteLine("Quantidade de Páginas Prontas: {0}", nTotalPaginas.Sum());
+                if (!itemDone)
+                    Console.WriteLine("Quantidade de Páginas Em Progresso: {0}", nWIP.Sum());
+
+                //Intervalo Total
+                TimeSpan difDateThroughput = dtEndThroughput - dtStartThroughput;
+                DateTimeOffset dtNow = DateTimeOffset.Now;
+                TimeSpan tsIntervaloTotal = DateTimeOffset.Now.AddHours(difDateThroughput.TotalHours - GetNumberOfWorkingHours(dtStartThroughput, dtEndThroughput)) - dtNow;
+
+                Console.WriteLine("Intervalo Total: {0}", tsIntervaloTotal.ToString());
+
+                //Throughput
+                Console.WriteLine("(Throughput) -> Média de {0} páginas por dia", Math.Round((nTotalPaginas.Sum() / tsIntervaloTotal.TotalHours) * 24, 2)); // TotalDeItens(Paginas) / TempoTotal
+
+                //CycleTime
+                TimeSpan tsTempoCycleTime = DateTimeOffset.Now.AddHours(Math.Round(tsIntervaloTotal.TotalHours / nTotalPaginas.Sum(), 2)) - dtNow;
+                Console.WriteLine("(CycleTime) -> Média de tempo gasto por página: {0}", tsTempoCycleTime.ToString());// ----TempoTotal / TotalDeItens(Paginas)
+                if (!itemDone)
                 {
-
-                    TimeSpan tsTempoPorPagina = DateTimeOffset.Now.AddHours(Convert.ToDouble(Math.Round(decTempoPorPagina.Average(), 2))) - DateTimeOffset.Now;
-
-                    Console.WriteLine("=======================================================");
-                    Console.WriteLine("De: {0} Até: {1}", startFieldValue, endFieldValue);
-                    Console.WriteLine("Quantidade de PBIs: {0}", decPaginasPorDia.Count);
-                    Console.WriteLine("Quantidade de Páginas: {0}", nTotalPaginas.Sum());
-                    Console.WriteLine("Média de tempo (em horas) gasto por página: {0}", tsTempoPorPagina.ToString());
-                    Console.WriteLine("Média de {0} páginas por dia", Math.Round(decPaginasPorDia.Average(), 2));
+                    TimeSpan tsTempoLeadTime = DateTimeOffset.Now.AddHours(Math.Round((nWIP.Sum() * tsTempoCycleTime.TotalHours), 2)) - dtNow;
+                    Console.WriteLine("(LeadTime) -> Tempo que uma página leva para ficar pronta: {0}", tsTempoLeadTime.ToString());//Media Geral ---"WIP * CycleTime" ou "WIP / Throughput
                 }
                 #endregion
             }
@@ -139,6 +177,8 @@ namespace KpiAgile
                 Console.WriteLine("No work items were returned from query.");
             }
             Console.ReadLine();
+            Console.Clear();
+            Program.Main(new string[0]);
         }
 
         private static int GetNumberOfWorkingHours(DateTime start, DateTime stop)
